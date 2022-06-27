f279b2f037b882d1642c7c79db9a69a46e332c8a
Prodipta <haldip9500@gmail.com>
2022-06-27 22:46:55 +0530

    debugger

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const mongoose = require("mongoose");
 //using dotenv module to access environment Variable
 require("dotenv").config();
 
+console.log(`${process.env.MONGO_URL}`);
 // DATABASE
 mongoose.connect(`${process.env.MONGO_URL}`,
 {
